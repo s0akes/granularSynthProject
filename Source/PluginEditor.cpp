f73805018bdd8f0759424337df3e5a29e1eaacc8
s0akes <94879083+s0akes@users.noreply.github.com>
2022-11-15 15:06:47 +0000

    Testing label and sliders
    Slider not currently functional :-1:

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -7,15 +7,15 @@
 */
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
-//hello
-//hi matthew oram - how's it going my slime
 //==============================================================================
 GranularSynthProjectAudioProcessorEditor::GranularSynthProjectAudioProcessorEditor (GranularSynthProjectAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+
+
+    setSize (210, 635);
 }
 
 GranularSynthProjectAudioProcessorEditor::~GranularSynthProjectAudioProcessorEditor()
@@ -26,15 +26,35 @@ GranularSynthProjectAudioProcessorEditor::~GranularSynthProjectAudioProcessorEdi
 void GranularSynthProjectAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    /*g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);*/
+
+    addAndMakeVisible(randomLabel); 
+    randomLabel.setFont(juce::Font(16.0f, juce::Font::bold));
+    randomLabel.setText("Random", juce::dontSendNotification);
+    randomLabel.setColour(juce::Label::textColourId, juce::Colours::lightseagreen);
+    randomLabel.setJustificationType(juce::Justification::centred);
+
+    addAndMakeVisible(randomSlider);
+    randomSlider.setRange(0.01, 10, 0.01);
+    randomSlider.setValue(1);
+    randomSlider.setSkewFactorFromMidPoint(1);
+    //randomSlider.addListener(this);
+    randomSlider.setSliderStyle(juce::Slider::SliderStyle::Rotary);
+    randomSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxBelow, false, 95, 28);
+    randomSlider.setColour(juce::Slider::rotarySliderFillColourId, juce::Colours::lightseagreen);
+
+
+
 }
 
 void GranularSynthProjectAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    randomLabel.setBounds(20, 5, 150, 25);
+    randomSlider.setBounds(20, 20, 100, 100);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -28,6 +28,8 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     GranularSynthProjectAudioProcessor& audioProcessor;
+    juce::Label randomLabel;
+    juce::Slider randomSlider;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GranularSynthProjectAudioProcessorEditor)
 };
