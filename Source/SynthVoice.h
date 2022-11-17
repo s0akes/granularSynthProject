bc1bcb5c277ca803eb7e4a35784a5d228fb77471
Bricklayer5 <114731706+Bricklayer5@users.noreply.github.com>
2022-11-17 16:43:37 +0000

    fixed

--- Source/SynthVoice.h
+++ Source/SynthVoice.h
@@ -17,3 +17,18 @@
 //add the samples together
 //output to buffer
 //done :)
+
+#include <JuceHeader.h>
+#include "waveTableClass.h"
+
+public:
+
+synthVoice(double frequency, int amplitude, double duration);
+
+
+private:
+
+juce::AudioSampleBuffer* WT = nullptr;
+double delta = 0;
+double currentIndex = 0;
+double amplitude = 0;
