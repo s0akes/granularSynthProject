78e5251527f6fe59602d267ae4b375b172d3b922
J-u-s-tD-a-n <110908827+J-u-s-tD-a-n@users.noreply.github.com>
2022-11-18 09:37:24 +0000

    more progress

--- Source/SynthVoice.cpp
+++ Source/SynthVoice.cpp
@@ -12,4 +12,46 @@
 
 synthVoice::synthVoice(double frequency, int amplitude, double duration){
   //take the noteon() and noteoff() data and send the respective amplitude, frequency and duration data on to the graining class
+  void  synthVoice (double freq, int ampl, double time){
+
+  }
+
+  bool  canPlaySound (juce::SynthesiserSound * sound){
+
+  }
+    
+  void  startNote (int midiNoteNumber, float velocity, juce::SynthesiserSound *sound, int currentPitchWheelPosition){
+
+    WT = &(dynamic_cast<sinSound*>(sound) -> waveTable);
+    
+    auto f = juce::MidiMessage::getMidiNoteInHertz(midiNoteNumber);
+    delta = WT->getNumSamples() * f / getSampleRate();
+    
+    amplitude = velocity * 0.2;
+     
+    currentIndex = 0;
+
+  }
+    
+  void  stopNote (float velocity, bool allowTailOff){
+
+    amplitude = 0.0;
+    clearCurrentNote();
+
+  }
+    
+  void  renderNextBlock (juce::AudioBuffer< float > &outputBuffer, int startSample, int numSamples){
+
+  }
+
 }
+
+
+//randomly trigger a grain store the grain in a vector
+//every sample check grain::isActive() 
+//if it is acitve grain::getNextSample
+//if it is not active then delete the grain
+//repeat for every grain in the vector
+//add the samples together
+//output to buffer
+//done :)
