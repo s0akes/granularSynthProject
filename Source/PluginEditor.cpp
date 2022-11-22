187502cb848db48b5295ff447aef18331f3e51d1
s0akes <94879083+s0akes@users.noreply.github.com>
2022-11-22 01:07:49 +0000

    created placeholder sliders for ADSR

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,22 +15,54 @@ GranularSynthProjectAudioProcessorEditor::GranularSynthProjectAudioProcessorEdit
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
 
-    setSize(210, 635);
+    setSize(735, 300); //sets size of window
 
-    addAndMakeVisible(&randomSlider);
-    addAndMakeVisible(randomLabel);
-
-    randomSlider.setRange(0.01, 10, 0.01);
+    randomSlider.setRange(0.01, 10, 0.01); //creates the slider "Random"
     randomSlider.setValue(1);
     randomSlider.setSkewFactorFromMidPoint(1);
     randomSlider.setSliderStyle(juce::Slider::SliderStyle::Rotary);
     randomSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxBelow, false, 95, 28);
     randomSlider.setColour(juce::Slider::rotarySliderFillColourId, juce::Colours::lightseagreen);
 
-    randomLabel.setFont(juce::Font(16.0f, juce::Font::bold));
+    attackSlider.setRange(0.01, 10, 0.01); //creates the slider "Attack"
+    attackSlider.setValue(1);
+    attackSlider.setSkewFactorFromMidPoint(1);
+    attackSlider.setSliderStyle(juce::Slider::SliderStyle::Rotary);
+    attackSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxBelow, false, 95, 28);
+
+    decaySlider.setRange(0.01, 10, 0.01); //creates the slider "Decay"
+    decaySlider.setValue(1);
+    decaySlider.setSkewFactorFromMidPoint(1);
+    decaySlider.setSliderStyle(juce::Slider::SliderStyle::Rotary);
+    decaySlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxBelow, false, 95, 28);
+
+    sussySlider.setRange(0.01, 10, 0.01); //creates the slider "Sustain"
+    sussySlider.setValue(1);
+    sussySlider.setSkewFactorFromMidPoint(1);
+    sussySlider.setSliderStyle(juce::Slider::SliderStyle::Rotary);
+    sussySlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxBelow, false, 95, 28);
+
+    releaseSlider.setRange(0.01, 10, 0.01); //creates the slider "Release"
+    releaseSlider.setValue(1);
+    releaseSlider.setSkewFactorFromMidPoint(1);
+    releaseSlider.setSliderStyle(juce::Slider::SliderStyle::Rotary);
+    releaseSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxBelow, false, 95, 28);
+
+
+
+
+    randomLabel.setFont(juce::Font(16.0f, juce::Font::bold)); //label for "Random"
     randomLabel.setText("Random", juce::dontSendNotification);
     randomLabel.setColour(juce::Label::textColourId, juce::Colours::lightseagreen);
     randomLabel.setJustificationType(juce::Justification::centred);
+
+    addAndMakeVisible(&randomSlider); //makes sliders visible
+    addAndMakeVisible(&attackSlider);
+    addAndMakeVisible(&decaySlider);
+    addAndMakeVisible(&sussySlider);
+    addAndMakeVisible(&releaseSlider);
+    
+    addAndMakeVisible(randomLabel); //makes labels visible
 }
 
 GranularSynthProjectAudioProcessorEditor::~GranularSynthProjectAudioProcessorEditor()
@@ -45,11 +77,18 @@ void GranularSynthProjectAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void GranularSynthProjectAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+
+    randomLabel.setBounds(20, 5, 95, 25);
+    
+    randomSlider.setBounds(20, 20, 95, 100);
+    attackSlider.setBounds(170, 20, 95, 100);
+    decaySlider.setBounds(320, 20, 95, 100);
+    sussySlider.setBounds(470, 20, 95, 100);
+    releaseSlider.setBounds(620, 20, 95, 100);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -29,7 +29,17 @@ private:
     // access the processor object that created it.
     GranularSynthProjectAudioProcessor& audioProcessor;
     juce::Label randomLabel;
+    juce::Label attackLabel;
+    juce::Label decayLabel;
+    juce::Label sussyLabel;
+    juce::Label releaseLabel;
+
     juce::Slider randomSlider;
+    juce::Slider attackSlider;
+    juce::Slider decaySlider;
+    juce::Slider sussySlider;
+    juce::Slider releaseSlider;
+
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GranularSynthProjectAudioProcessorEditor)
 };
